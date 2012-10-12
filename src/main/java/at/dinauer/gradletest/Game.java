65ff65ce17c6b6df96847481a6bcaa7b807bb83f
adinauer <alex@adnb.(none)>
2012-10-12 13:58:04 +0200

    initial commit of gradle project

---
+++ src/main/java/at/dinauer/gradletest/Game.java
@@ -0,0 +1,6 @@
+package at.dinauer.gradletest;
+
+public class Game
+{
+
+}
---
+++ src/test/java/at/dinauer/gradletest/GameTest.java
@@ -0,0 +1,15 @@
+package at.dinauer.gradletest;
+
+
+import org.junit.Test;
+
+
+public class GameTest
+{
+
+    @Test
+    public void test() {
+        Game game = new Game();
+    }
+
+}
